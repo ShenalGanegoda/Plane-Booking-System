004efd5439e67cd5ab0364ac346edea3af83cd34
ShenalGanegoda <106686816+ShenalGanegoda@users.noreply.github.com>
2024-05-14 10:20:52 +0530

    Method - buyASeat, start

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     private static Scanner scn = new Scanner(System.in); // Creating a Scanner object for inputs.
+    private static int[] A = {0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0};
+    private static int[] B = {0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 };
+    private static int[] C = {0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 };
+    private static int[] D = {0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0};
 
     public static void main(String[] args) {
-
         while (true) {
             methodChoose(menuOption());
         }
@@ -44,11 +47,39 @@ public class Main {
 
         }
     }
-    private static void buyASeat(){};
+    private static void buyASeat(){
+        // Separate method to buy and reserve a seat.
+        String rowChosen = rowLetterInput();
+        int seatNumberChosen = seatNumberInput();
+
+
+
+    }
     private static void cancelASeat(){};
     private static void findFirstSeatAvailable(){};
     private static void showSeatingPlan(){};
     private static void printTicketInfoAndPrice(){};
     private static void searchTicket(){};
     private static void quit(){};
+
+
+    // Other methods
+
+    private static String rowLetterInput(){ // Separate method to get row letter input.
+        System.out.print("Enter row here: ");
+        return scn.nextLine();
+    }
+
+    private static int seatNumberInput(){ // Separate method to get seat number input.
+        System.out.print("Enter seat number here: ");
+        return scn.nextInt();
+    }
+
+    private static boolean rowLetterValidCheck(){ // Separate method to validate row letter.
+     return true;
+    }
+
+    private static boolean seatValidCheck(int seatNumber){ // Separate method to validate seat number.
+        return true;
+    }
 }
